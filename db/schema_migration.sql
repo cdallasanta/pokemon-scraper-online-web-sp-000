e447b004068be7a74146a5b9a709c6d8df4d091d
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-04-15 22:24:31 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,11 +1,7 @@
 -- Write your sql commands to create your db schema here.
 -- It should create a table in a new table in the db/ directory.
 
-<<<<<<< HEAD
 CREATE TABLE pokemon (
-=======
-CREATE TABLE pokemon(
->>>>>>> 319104c22fa9571276b03f0c2a61377ded801f99
   id INTEGER PRIMARY KEY,
   name TEXT,
   type TEXT
